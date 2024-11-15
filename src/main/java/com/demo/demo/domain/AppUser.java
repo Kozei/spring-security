dfccfd8c90db491e5adb6ec7414318d773e7cb3f
Zourkas <zourkas@hotmail.gr>
2024-11-15 12:00:31 +0100

    Add code styles config

--- src/main/java/com/demo/demo/domain/AppUser.java
+++ src/main/java/com/demo/demo/domain/AppUser.java
@@ -1,11 +1,17 @@
 package com.demo.demo.domain;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Collection;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 @Data
